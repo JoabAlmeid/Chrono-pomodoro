318bb6729a80cb49d58f73077c1b36b92d5b9efa
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-21 20:51:41 -0300

    changing theme icons

--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,11 +1,26 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
 
 type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>("dark");
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
@@ -18,27 +33,13 @@ export function Menu() {
     });
   }
 
-  // useEffect(() => {
-  //   console.log("useEffect sem dependência", Date.now());
-  // }); //executa toda vez que o componente renderiza na tela
-
-  // useEffect(() => {
-  //   console.log("", Date.now());
-  // }, []);//executa apenas quando o react executa o componente na tela na primeira vez
-
   useEffect(() => {
-    console.log("", Date.now());
     document.documentElement.setAttribute("data-theme", theme);
-
-    //função de cleanup. Executa antes do useEffect todo para tirar o previous state
-    return () => {
-      console.log("Este componente será atualizado");
-    };
+    localStorage.setItem("theme", theme);
   }, [theme]); //executa toda vez que o valor da dependência mudar, ou seja, o [theme]
 
   return (
     <nav className={styles.menu}>
-      <h1>{theme}</h1>
       <a
         className={styles.menuLink}
         href="#"
@@ -70,7 +71,7 @@ export function Menu() {
         title="Mudar Tema"
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
