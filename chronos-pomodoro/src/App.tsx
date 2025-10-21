a3c9fd94f319736beb35a5800b26b74dcc0b0181
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-21 20:39:52 -0300

    light and dark themes

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -10,28 +10,10 @@ import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
 import { Footer } from "./components/Footer";
-import { Heading } from "./components/Heading";
 
 export function App() {
-  let numero = 0;
-
-  function handleClick() {
-    const span = document.getElementById("numero");
-
-    if (!span) return;
-
-    numero += 1;
-    span.innerText = numero.toString();
-    console.log(numero, Date.now());
-  }
-
   return (
     <>
-      <Heading>
-        Número: <span id="numero">{numero}</span>
-      </Heading>
-
-      <button onClick={handleClick}>Aumenta</button>
       <Container>
         <Logo />
       </Container>
@@ -45,7 +27,7 @@ export function App() {
         <form className="form " action="">
           <div className="formRow">
             <DefaultInput
-              labelText={numero.toString()}
+              labelText="task"
               id="meuInput"
               type="text"
               placeholder="Digite algo"
--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,19 +1,75 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  // useEffect(() => {
+  //   console.log("useEffect sem dependência", Date.now());
+  // }); //executa toda vez que o componente renderiza na tela
+
+  // useEffect(() => {
+  //   console.log("", Date.now());
+  // }, []);//executa apenas quando o react executa o componente na tela na primeira vez
+
+  useEffect(() => {
+    console.log("", Date.now());
+    document.documentElement.setAttribute("data-theme", theme);
+
+    //função de cleanup. Executa antes do useEffect todo para tirar o previous state
+    return () => {
+      console.log("Este componente será atualizado");
+    };
+  }, [theme]); //executa toda vez que o valor da dependência mudar, ou seja, o [theme]
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <h1>{theme}</h1>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a Home"
+        title="Ir para a Home"
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ver Histórico"
+        title="Ver Histórico"
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para Configurações"
+        title="Ir para a Configurações"
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar Tema"
+        title="Mudar Tema"
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
