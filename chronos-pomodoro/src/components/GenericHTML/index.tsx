cde730565caacf456a5e308e85c4bf9ea1feb6c7
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-22 21:10:35 -0300

    aboutPomodoro and notfound pages created

---
+++ chronos-pomodoro/src/components/GenericHTML/index.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+import styles from "./styles.module.css";
+
+type GenericHTMLProps = {
+  children: React.ReactNode;
+};
+
+export function GenericHTML({ children }: GenericHTMLProps) {
+  return <div className={styles.genericHtml}>{children}</div>;
+}
