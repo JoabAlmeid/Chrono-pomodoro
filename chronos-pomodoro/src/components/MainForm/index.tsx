6cf6328ba0d0ca53f3a8a4400da5ad8e68126614
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-23 19:11:30 -0300

    prop drilling study

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -1,11 +1,32 @@
-import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { PlayCircleIcon } from "lucide-react";
+import type { HomeProps } from "../../pages/Home";
 
-export function MainForm() {
+export function MainForm({ state, setState }: HomeProps) {
+  function handleClick() {
+    setState((prevState) => {
+      return {
+        ...prevState,
+
+        config: {
+          ...prevState.config,
+
+          workTime: 34,
+        },
+
+        formattedSecondsRemaining: "23:34",
+      };
+    });
+  }
   return (
-    <form className="form " action="">
+    <form className="form" action="">
+      <div>
+        <button type="button" onClick={handleClick}>
+          Clicar
+        </button>
+      </div>
       <div className="formRow">
         <DefaultInput
           labelText="task"
@@ -16,7 +37,7 @@ export function MainForm() {
       </div>
 
       <div className="formRow">
-        <p>Lorem ipsum dolor sit amet.</p>
+        <p>O próximo interválo é de {state.config.workTime}min</p>
       </div>
 
       <div className="formRow">
