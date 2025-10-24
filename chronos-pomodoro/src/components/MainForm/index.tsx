2b942093d9898316dfa6009c53482f4ff8a56756
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-23 22:43:29 -0300

    context API provider

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -2,31 +2,10 @@ import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { PlayCircleIcon } from "lucide-react";
-import type { HomeProps } from "../../pages/Home";
 
-export function MainForm({ state, setState }: HomeProps) {
-  function handleClick() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-
-        config: {
-          ...prevState.config,
-
-          workTime: 34,
-        },
-
-        formattedSecondsRemaining: "23:34",
-      };
-    });
-  }
+export function MainForm() {
   return (
     <form className="form" action="">
-      <div>
-        <button type="button" onClick={handleClick}>
-          Clicar
-        </button>
-      </div>
       <div className="formRow">
         <DefaultInput
           labelText="task"
@@ -37,7 +16,7 @@ export function MainForm({ state, setState }: HomeProps) {
       </div>
 
       <div className="formRow">
-        <p>O próximo interválo é de {state.config.workTime}min</p>
+        <p>O próximo interválo é de 25min</p>
       </div>
 
       <div className="formRow">
