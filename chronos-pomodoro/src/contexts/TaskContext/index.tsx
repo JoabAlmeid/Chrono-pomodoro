05e91efe0401a7b01597bc031203e020d856414b
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-27 20:20:49 -0300

    useState and provider

--- chronos-pomodoro/src/contexts/TaskContext/index.tsx
+++ chronos-pomodoro/src/contexts/TaskContext/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import type { TaskStateModel } from "../../models/TaskStateModel";
 
 const initialState: TaskStateModelteModel = {
@@ -31,8 +31,10 @@ type TaskContextProviderProps = {
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
+  const [state, setState] = useState(initialState);
+
   return (
-    <TaskContext.Provider value={{ ...initialContextValue }}>
+    <TaskContext.Provider value={{ state, setState }}>
       {children}
     </TaskContext.Provider>
   );
