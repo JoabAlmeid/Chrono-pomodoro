fef34bb29afe781666a2e47ae907bf26ec0bffe8
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-28 20:32:49 -0300

    validating form and creating new task

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -4,14 +4,48 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { PlayCircleIcon } from "lucide-react";
 import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log("deu certo");
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, //conferir depois
+        secondsRemaining, //conferir depois talvez
+        formattedSecondsRemaining: "00:00", //conferir depois
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
