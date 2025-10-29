ddc28788c6e72326b5123e63e973338e14f9a5eb
Joab Almeida <joabalmeida.dev@gmail.com>
2025-10-29 18:42:29 -0300

    cycles working

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -72,11 +72,11 @@ export function MainForm() {
       <div className="formRow">
         <p>O próximo interválo é de 25min</p>
       </div>
-
-      <div className="formRow">
-        <Cycles />
-      </div>
-
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} />
       </div>
